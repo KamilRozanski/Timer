2a132bc428d3e23e47cf8b79ac80919eef497924
Kamil Rozanski <motomc1@gmail.com>
2020-10-25 23:58:19 +0000

    new date

--- main.js
+++ main.js
@@ -1,15 +1,15 @@
-const nowTime = new Date("2019/10/03 22:53:00").getTime();
-const endTime = new Date("2019/10/11 0:00:00").getTime();
+const nowTime = new Date("2020/25/03 22:53:00").getTime();
+const endTime = new Date("2021/10/11 0:00:00").getTime();
 const spanDays = document.querySelector('span.d');
 const spanHours = document.querySelector('span.h');
 const spanMinutes = document.querySelector('span.m');
 const spanSeconds = document.querySelector('span.s');
 
 
-setInterval(()=>{
+setInterval(() => {
     const nowTime = new Date().getTime(); //get a current time
-    
-    
+
+
     let days = Math.floor((endTime / (1000 * 60 * 60 * 24)) - (nowTime / (1000 * 60 * 60 * 24)));
     // 1000s (micro sec), 60m (minuts), 60s (seconds) 24h (hours) = days
     days = days < 10 ? `0${days}` : days;
@@ -19,7 +19,7 @@ setInterval(()=>{
     hours = hours < 10 ? `0${hours}` : hours; // add "0" if hours < 10.
 
     let minutes = Math.floor((endTime / (1000 * 60) - nowTime / (1000 * 60))) % 60;
-    minutes = minutes < 10 ? `0${minutes}` : minutes;// add "0" if minutes < 10.
+    minutes = minutes < 10 ? `0${minutes}` : minutes; // add "0" if minutes < 10.
 
     let seconds = Math.floor((endTime / 1000 - nowTime / 1000)) % 60;
     seconds = seconds < 10 ? `0${seconds}` : seconds; // add "0" if seconds < 10.
