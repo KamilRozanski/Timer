3d00bf7c4b741017c8f286ab41290ae899ac6ce3
KamilRozanski <motomc1@gmail.com>
2021-10-12 11:10:50 +0100

    timer has 24h clock

--- main.js
+++ main.js
@@ -1,28 +1,49 @@
-const nowTime = new Date("2020/25/03 22:53:00").getTime();
-const endTime = new Date("2021/10/11 0:00:00").getTime();
+"use strict";
+
+// service worker registration - remove if you're not going to use it
+
+if ('serviceWorker' in navigator) {
+    window.addEventListener('load', function () {
+        navigator.serviceWorker.register('serviceworker.js').then(function (registration) {
+            // Registration was successful
+            console.log('ServiceWorker registration successful with scope: ', registration.scope);
+        }, function (err) {
+            // registration failed :(
+            console.log('ServiceWorker registration failed: ', err);
+        });
+    });
+}
+
+// place your code below
+
+
+let nowTime = new Date("2020/10/25 00:00:01").getTime();
+let endTime = new Date("2021/10/18 00:00:00").getTime();
 const spanDays = document.querySelector('span.d');
 const spanHours = document.querySelector('span.h');
 const spanMinutes = document.querySelector('span.m');
 const spanSeconds = document.querySelector('span.s');
+const mainParagraph = document.querySelector('.main__paragraph');
 
 
 setInterval(() => {
-    const nowTime = new Date().getTime(); //get a current time
+
+    let nowTime = new Date().getTime(); //get a current time
 
 
     let days = Math.floor((endTime / (1000 * 60 * 60 * 24)) - (nowTime / (1000 * 60 * 60 * 24)));
     // 1000s (micro sec), 60m (minuts), 60s (seconds) 24h (hours) = days
-    days = days < 10 ? `0${days}` : days;
+    days = days < 10 && days >= 0 ? `0${days}` : days;
 
 
     let hours = Math.floor((endTime / (1000 * 60 * 60) - nowTime / (1000 * 60 * 60))) % 24;
-    hours = hours < 10 ? `0${hours}` : hours; // add "0" if hours < 10.
+    hours = hours < 10 && hours >= 0 ? `0${hours}` : hours; // add "0" if hours < 10.
 
     let minutes = Math.floor((endTime / (1000 * 60) - nowTime / (1000 * 60))) % 60;
-    minutes = minutes < 10 ? `0${minutes}` : minutes; // add "0" if minutes < 10.
+    minutes = minutes < 10 && minutes >= 0 ? `0${minutes}` : minutes; // add "0" if minutes < 10.
 
     let seconds = Math.floor((endTime / 1000 - nowTime / 1000)) % 60;
-    seconds = seconds < 10 ? `0${seconds}` : seconds; // add "0" if seconds < 10.
+    seconds = seconds < 10 && seconds >= 0 ? `0${seconds}` : seconds; // add "0" if seconds < 10.
 
 
     spanDays.innerHTML = days;
@@ -30,7 +51,34 @@ setInterval(() => {
     spanMinutes.innerHTML = minutes;
     spanSeconds.innerHTML = seconds;
 
+    if (endTime < nowTime) {
+        let days = Math.floor((nowTime / (1000 * 60 * 60 * 24)) - (endTime / (1000 * 60 * 60 * 24)));
+        // 1000s (micro sec), 60m (minuts), 60s (seconds) 24h (hours) = days
+        days = days < 10 && days >= 0 ? `0${days}` : days;
+
+        let hours = Math.floor((nowTime / (1000 * 60 * 60) - endTime / (1000 * 60 * 60))) % 24;
+        hours = hours < 10 && hours >= 0 ? `0${hours}` : hours; // add "0" if hours < 10.
+
+        let minutes = Math.floor((nowTime / (1000 * 60) - endTime / (1000 * 60))) % 60;
+        minutes = minutes < 10 && minutes >= 0 ? `0${minutes}` : minutes; // add "0" if minutes < 10.
+        let seconds = Math.floor((nowTime / 1000 - endTime / 1000)) % 60;
+        seconds = seconds < 10 && seconds >= 0 ? `0${seconds}` : seconds; // add "0" if seconds < 10.
+
+        spanDays.innerHTML = `I'm late<br> ${days}`;
+        spanHours.innerHTML = hours;
+        spanMinutes.innerHTML = minutes;
+        spanSeconds.innerHTML = seconds;
+
+        mainParagraph.innerHTML = `Upss I'm a bit late, but don't worry soon you will see me 😍`;
+
+    } else {
+        mainParagraph.innerHTML = "Alan's birthday will be 😍";
+    }
+
+
 }, 1000)
+
+
 
 
 // // Set the date we're counting down to
