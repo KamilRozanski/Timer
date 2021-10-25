f2613228f7fc8152ee2b51cf55d2cb28582e79ab
KamilRozanski <motomc1@gmail.com>
2021-10-25 13:10:43 +0100

    years are updating atomaticly

--- main.js
+++ main.js
@@ -33,9 +33,7 @@ let alanBirthaday = 0
 setInterval(() => {
 
     let nowTime = new Date().getTime(); //get a current time
-    let today = new Date();
-    let yr = today.getFullYear();
-    alanBirthaday = (yr - 2019)
+
 
 
 
@@ -62,38 +60,11 @@ setInterval(() => {
     spanMinutes.innerHTML = minutes;
     spanSeconds.innerHTML = seconds;
 
-
-
-    // console.log(endTime < nowTime)
-    if (endTime < nowTime) {
-
-
-
-
-        // let days = Math.floor((nowTime / (1000 * 60 * 60 * 24)) - (endTime / (1000 * 60 * 60 * 24)));
-        // // 1000s (micro sec), 60m (minuts), 60s (seconds) 24h (hours) = days
-        // days = days < 10 && days >= 0 ? `0${days}` : days;
-
-        // let hours = Math.floor((nowTime / (1000 * 60 * 60) - endTime / (1000 * 60 * 60))) % 24;
-        // hours = hours < 10 && hours >= 0 ? `0${hours}` : hours; // add "0" if hours < 10.
-
-        // let minutes = Math.floor((nowTime / (1000 * 60) - endTime / (1000 * 60))) % 60;
-        // minutes = minutes < 10 && minutes >= 0 ? `0${minutes}` : minutes; // add "0" if minutes < 10.
-        // let seconds = Math.floor((nowTime / 1000 - endTime / 1000)) % 60;
-        // seconds = seconds < 10 && seconds >= 0 ? `0${seconds}` : seconds; // add "0" if seconds < 10.
-
-        // spanDays.innerHTML = `I'm late<br> ${days}`;
-        // spanHours.innerHTML = hours;
-        // spanMinutes.innerHTML = minutes;
-        // spanSeconds.innerHTML = seconds;
-
-        // mainParagraph.innerHTML = `Upss I'm a bit late, but don't worry soon you will see me 😍`;
-
-
-    } else {
-
-        alanBirthaday += 1
+    alanBirthaday = (currentYear - 2019)
+    if (alanBirthaday == 3) {
         mainParagraph.innerHTML = `Alan's ${alanBirthaday}rd  birthday will be 😍`;
+    } else {
+        mainParagraph.innerHTML = `Alan's ${alanBirthaday}th  birthday will be 😍`;
     }
 
 
